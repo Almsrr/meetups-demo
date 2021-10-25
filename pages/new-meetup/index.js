3a19f786d03f53c5a665d879fa6892035768a824
Alam <alam.15@outlook.com>
2021-10-25 12:20:17 -0400

    new meetup page

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,30 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import axios from "axios";
+
+import NewMeetupFrom from "../../components/meetups/NewMeetupForm";
+
+function NewMeetup() {
+  const router = useRouter();
+
+  const addMeetupHandler = async (newMeetup) => {
+    const response = await axios.post("/api/meetups", newMeetup);
+
+    if (response.status === 201) {
+      alert("Meetup inserted successfully");
+      router.push("/");
+    }
+  };
+  return (
+    <Fragment>
+      <Head>
+        <title>New Meetup</title>
+        <meta name="description" content="You can add our first meetup" />
+      </Head>{" "}
+      <NewMeetupFrom onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
+}
+
+export default NewMeetup;
